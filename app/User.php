93e024730be5055d71d8b12f054c0ddcf4a22603
alon <alon@my-dev.io>
2020-04-14 18:42:57 +0300

    add welcome mail when a user registers

--- app/User.php
+++ app/User.php
@@ -2,10 +2,12 @@
 
 namespace App;
 
+use App\Mail\NewUserWelcomeMail;
 use function foo\func;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
 
 class User extends Authenticatable
 {
@@ -48,6 +50,8 @@ class User extends Authenticatable
             $user->profile()->create([
                 'title' => $user->username,
             ]);
+
+            Mail::to($user->email)->send(new NewUserWelcomeMail());
         });
     }
 
